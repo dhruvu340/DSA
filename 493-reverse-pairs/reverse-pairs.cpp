bf54f8d39ae4ff188c81a4db55eaddad625938f8
dhruvu340 <dhruvu3400@gmail.com>
2026-01-30 11:48:34 +0530

    Time: 495 ms (53.19%) | Memory: 243.6 MB (14.39%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.cpp
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -1,60 +1,43 @@
 class Solution {
 public:
-void merge(vector<int>& vec, int left, int mid, int right) {
-    int i, j, k;
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    // Create temporary vectors
-    vector<int> leftVec(n1), rightVec(n2);
-
-    // Copy data to temporary vectors
-    for (i = 0; i < n1; i++)
-        leftVec[i] = vec[left + i];
-    for (j = 0; j < n2; j++)
-        rightVec[j] = vec[mid + 1 + j];
-
-    // Merge the temporary vectors back into vec[left..right]
-    i = 0;
-    j = 0;
-    k = left;
-    while (i < n1 && j < n2) {
-        if (leftVec[i] <= rightVec[j]) {
-            vec[k] = leftVec[i];
+void merge(vector<int>& nums,int l,int mid,int r){
+    vector<int>temp;
+    int i=l;
+    int j=mid+1;
+    while(i<=mid&&j<=r){
+        if(nums[i]<=nums[j]){
+            temp.push_back(nums[i]);
             i++;
-        } else {
-            vec[k] = rightVec[j];
-            j++;
+        }else{
+            temp.push_back(nums[j++]);
         }
-        k++;
     }
 
-    // Copy the remaining elements of leftVec[], if any
-    while (i < n1) {
-        vec[k] = leftVec[i];
-        i++;
-        k++;
+    while(i<=mid){
+         temp.push_back(nums[i++]);
     }
 
-    // Copy the remaining elements of rightVec[], if any
-    while (j < n2) {
-        vec[k] = rightVec[j];
-        j++;
-        k++;
+
+    while(j<=r){
+        temp.push_back(nums[j++]);
     }
+
+    for(int i=0;i<temp.size();i++){
+        nums[i+l]=temp[i];
+    }
+
 }
 
 
-int countpair(vector<int>& vec, int left, int mid, int right){
+int countpair(vector<int>& nums,int l,int mid,int r){
     int ct=0;
-    int right1=mid+1;
-    for(int i=left;i<=mid;i++){
-        while(right1<=right&&(vec[i]>2LL*(vec[right1]))){right1++;}
-        ct+=right1-(mid+1);
+    int j=mid+1;
+    for(int i=l;i<=mid;i++){
+        while(j<=r&&(nums[i]>(2LL*nums[j]))){j++;}
+        ct+=j-(mid+1);
     }
     return ct;
 }
-
     int mergesort(vector<int>& nums,int l,int r){
         int ct=0;
         if(l<r){
