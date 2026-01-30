e4fa30231c942e31f9f5a26c83eb191fc28c82e2
dhruvu340 <dhruvu3400@gmail.com>
2026-01-30 10:56:46 +0530

    Time: 372 ms (69.09%) | Memory: 148.6 MB (69.11%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.cpp
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -1,58 +1,74 @@
 class Solution {
 public:
-    void merge(vector<int>& vec, int left, int mid, int right) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
+void merge(vector<int>& vec, int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
 
-        vector<int> leftVec(n1), rightVec(n2);
+    // Create temporary vectors
+    vector<int> leftVec(n1), rightVec(n2);
 
-        for (int i = 0; i < n1; i++)
-            leftVec[i] = vec[left + i];
-        for (int j = 0; j < n2; j++)
-            rightVec[j] = vec[mid + 1 + j];
+    // Copy data to temporary vectors
+    for (i = 0; i < n1; i++)
+        leftVec[i] = vec[left + i];
+    for (j = 0; j < n2; j++)
+        rightVec[j] = vec[mid + 1 + j];
 
-        int i = 0, j = 0, k = left;
-        while (i < n1 && j < n2) {
-            if (leftVec[i] <= rightVec[j])
-                vec[k++] = leftVec[i++];
-            else
-                vec[k++] = rightVec[j++];
+    // Merge the temporary vectors back into vec[left..right]
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2) {
+        if (leftVec[i] <= rightVec[j]) {
+            vec[k] = leftVec[i];
+            i++;
+        } else {
+            vec[k] = rightVec[j];
+            j++;
         }
-
-        while (i < n1)
-            vec[k++] = leftVec[i++];
-
-        while (j < n2)
-            vec[k++] = rightVec[j++];
+        k++;
     }
 
-    int countReversePairsInRange(vector<int>& vec, int left, int mid, int right) {
-        int count = 0;
-        int rightIndex = mid + 1;
-
-        for (int i = left; i <= mid; i++) {
-            while (rightIndex <= right &&
-                   (long long)vec[i] > 2LL * vec[rightIndex]) {
-                rightIndex++;
-            }
-            count += rightIndex - (mid + 1);
-        }
-        return count;
+    // Copy the remaining elements of leftVec[], if any
+    while (i < n1) {
+        vec[k] = leftVec[i];
+        i++;
+        k++;
     }
 
-    int mergesort(vector<int>& nums, int l, int r) {
-        int ct = 0;
-        if (l < r) {
-            int mid = l + (r - l) / 2;
-            ct += mergesort(nums, l, mid);
-            ct += mergesort(nums, mid + 1, r);
-            ct += countReversePairsInRange(nums, l, mid, r);
-            merge(nums, l, mid, r);
+    // Copy the remaining elements of rightVec[], if any
+    while (j < n2) {
+        vec[k] = rightVec[j];
+        j++;
+        k++;
+    }
+}
+
+
+int countpair(vector<int>& vec, int left, int mid, int right){
+    int ct=0;
+    int right1=mid+1;
+    for(int i=left;i<=mid;i++){
+        while(right1<=right&&(vec[i]>2LL*(vec[right1]))){right1++;}
+        ct+=right1-(mid+1);
+    }
+    return ct;
+}
+
+    int mergesort(vector<int>& nums,int l,int r){
+        int ct=0;
+        if(l<r){
+            int mid=l+(r-l)/2;
+           ct+= mergesort(nums,l,mid);
+           ct+= mergesort(nums,mid+1,r);
+           ct+= countpair(nums,l,mid,r);
+           merge(nums,l,mid,r);
         }
+
         return ct;
     }
-
     int reversePairs(vector<int>& nums) {
-        return mergesort(nums, 0, nums.size() - 1);
+        int n=nums.size();
+      return  mergesort(nums,0,n-1);
     }
 };
