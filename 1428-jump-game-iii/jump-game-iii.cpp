ee4339b9c7bec349181e4333480a77b83e639dec
dhruvu340 <dhruvu3400@gmail.com>
2026-03-10 02:34:37 +0530

    Time: 0 ms (100.00%) | Memory: 35.3 MB (85.41%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    bool solve(vector<int>&arr,int i){
-        if(i>=arr.size()||i<0){
-            return false;
+    
+    bool canReach(vector<int>& arr, int start) { 
+        queue<int>q;
+        q.push(start);
+        while(!q.empty()){
+            int idx=q.front();
+            q.pop();
+            if(idx<0||idx>=arr.size()||arr[idx]<0)continue;
+           
+            if(arr[idx]==0)return true;
+            int jump=arr[idx];
+            arr[idx]=-1;
+            q.push(idx+jump);
+            q.push(idx-jump);
+            
+            
         }
 
-        if(arr[i]==0){
-            return true;
-        }
-        int idx=arr[i];
-        
-        if(arr[i]!=-1){
-            arr[i]=-1;
-            return solve(arr,i+idx) || solve(arr,i-idx);
-            
-        }else{
-            return false;
-        }
-    }
-    bool canReach(vector<int>& arr, int start) { return solve(arr, start); }
+        return false;
+     }
 };
