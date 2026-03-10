6bb86bcb9440c68641c95f11feb8697a9a7c1389
dhruvu340 <dhruvu3400@gmail.com>
2026-03-10 20:25:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (79.58%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int>dp=vector<int>(20,-1);
+    int numTrees(int n) {
+        if(n<=1)return 1;
+        if(dp[n]!=-1)return dp[n];
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            ans+=(numTrees(i-1)*numTrees(n-i));
+        }
+        return dp[n]=ans;
+    }
+};
