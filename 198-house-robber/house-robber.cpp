bd581f6cc0d61ff916fa5cad569ce841fcf3121d
dhruvu340 <dhruvu3400@gmail.com>
2026-03-14 03:23:45 +0530

    Time: 3 ms (6.21%) | Memory: 10.6 MB (43.88%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -7,9 +7,9 @@ public:
     dp[0]=nums[0];
     dp[1]=max(dp[0],nums[1]);
     for(int i=2;i<n;i++){
-        dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
-
+        dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
     }
+
 
     return dp[n-1];
 
