69b66a8d7097e085e893e75c50102386bb873733
dhruvu340 <dhruvu3400@gmail.com>
2026-03-19 04:47:30 +0530

    Time: 200 ms (11.28%) | Memory: 42.7 MB (46.42%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    int dp[10001][201];
-    bool solve(vector<int>&nums,int ns,int i,int &sum){
-        if(ns==(sum/2))return true;
-        if(i>=nums.size()||ns>(sum/2))return false;
-        if(dp[ns][i]!=-1)return dp[ns][i];
-
-        return dp[ns][i] = solve(nums,ns+nums[i],i+1,sum) || solve(nums,ns,i+1,sum);
-    }
+    
     bool canPartition(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
+        int n=nums.size();
         int sum=accumulate(nums.begin(),nums.end(),0);
         if(sum&1)return false;
-        return solve(nums,0,0,sum);
+        vector<vector<bool>>dp(sum+1,vector<bool>(n+1,false));
+        for(int i=0;i<=n;i++){
+            dp[0][i]=true;
+        }
+
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum/2;j++){
+                if(j-nums[i-1]>=0){
+                    dp[j][i]=dp[j-nums[i-1]][i-1]||dp[j][i-1];
+                }else{
+                    dp[j][i]=dp[j][i-1];
+                }
+            }
+        }
+
+
+
+        return dp[sum/2][n];
+
     }
 };
