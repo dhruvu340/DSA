cd77aef929001b938b5f0e4887d487a46bde6c19
dhruvu340 <dhruvu3400@gmail.com>
2026-03-25 01:09:34 +0530

    Time: 11 ms (12.39%) | Memory: 27 MB (5.52%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
-    bool solve(vector<vector<char>>& board,int target , int n,int m,int row,int col){
-        for(int i=0;i<m;i++){
-            if(i==col)continue;
-
-            if(board[row][i]==target)return false;
-        }
-
-        for(int i=0;i<n;i++){
-            if(i==row)continue;
-
-            if(board[i][col]==target)return false;
-        }
-        // row/3 denotes the number of that cell in order to find position in grid of 3 and after finding the number we multiply * 3 to find actual position
-
-       int sr=(row/3)*3;
-       int sc=(col/3)*3;
-       for(int i=sr;i<sr+3;i++){
-        for(int j=sc;j<sc+3;j++){
-            if(i==row&&j==col)continue;
-            if(board[i][j]==target)return false;
-        }
-       }
-
-return true;
-
-    }
+   
     bool isValidSudoku(vector<vector<char>>& board) {
-        int n=board.size();
-        int m=board[0].size();
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-                if(board[i][j]!='.'&& solve(board,board[i][j],n,m,i,j)==false){
+        vector<unordered_set<int>>row(9),col(9);
+
+        unordered_map<string,unordered_set<int>>box;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.')continue;
+                string t= to_string(i/3) + "#" + to_string(j/3);
+
+                if(row[i].count(board[i][j])||col[j].count(board[i][j])||box[t].count(board[i][j])){
                     return false;
                 }
+
+                row[i].insert(board[i][j]);
+                col[j].insert(board[i][j]);
+                box[t].insert(board[i][j]);
             }
         }
 
+
         return true;
+        
     }
 };
