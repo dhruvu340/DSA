3194e3c1a79b42cb6c99c9e6b1ae0a4c12f4078e
dhruvu340 <dhruvu3400@gmail.com>
2026-03-24 19:05:19 +0530

    Time: 42 ms (87.06%) | Memory: 29.1 MB (71.95%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,23 +4,23 @@ public:
         vector<vector<int>>ans;
         int n=nums.size();
         sort(begin(nums),end(nums));
-        for(int i=0;i<n-2;i++){
-            if(i>0&&nums[i]==nums[i-1]){continue;}
-            int j=i+1;
-            int k=n-1;
-            while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum<0){
-                    j++;
-                }else if(sum>0){
-                    k--;
-                }else{
-                    ans.push_back({nums[i],nums[j++],nums[k--]});
-                    while (j < k && nums[j] == nums[j - 1]) j++;
-                    
-                }
+       for(int i=0;i<n-2;i++){
+        if(i-1>=0&&nums[i]==nums[i-1])continue;
+        int j=n-1;
+        int k=i+1;
+        while(k<j){
+            int sum=nums[i]+nums[j]+nums[k];
+            if(sum<0){
+                k++;
+            }else if(sum>0){
+                j--;
+            }else{
+                ans.push_back({nums[i],nums[k++],nums[j--]});
+                while(k<j&&nums[k]==nums[k-1])k++;
+                while(k<j&&nums[j]==nums[j+1])j--;
             }
         }
+       }
 
 
         
