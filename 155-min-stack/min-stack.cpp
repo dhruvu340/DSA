f4a874cd997b1480da2f8c5f14c264e8554d9623
dhruvu340 <dhruvu3400@gmail.com>
2026-03-25 10:43:37 +0530

    Time: 1 ms (73.45%) | Memory: 23.4 MB (50.91%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,52 +1,51 @@
 class MinStack {
 public:
-    stack<long long> s;
-    long long minval;
-
-    MinStack() {}
-
+    stack<long long>s;
+    long long int minval;
+    MinStack() {
+        
+    }
+    
     void push(int val) {
-        if (s.empty()) {
-            minval = val;
+       if(s.empty()){
+        minval=val;
+        s.push(val);
+       }else{
+        if(val>=minval){
             s.push(val);
-        } else {
-            if (val >= minval) {
-                s.push(val);
-            } else {
-                s.push(2LL * val - minval);
-                minval = val;
-            }
+        }else{
+           
+            s.push(2LL*val-minval);
+             minval=val;
         }
-    }
 
+        
+       }
+    }
+    
     void pop() {
-        if (s.empty()) return;
-
-        long long x = s.top();
+        if(s.empty())return ;
+        long long x=s.top();
         s.pop();
+        if(x<minval){
+            minval=2LL*minval-x;
 
-        if (x < minval) {
-            minval = 2LL * minval - x;
-        }
-
-        // ✅ IMPORTANT FIX
-        if (s.empty()) {
-            minval = -1; // or any dummy value
         }
     }
-
+    
     int top() {
-        if (s.empty()) return -1;
-
-        if (s.top() < minval) return minval;
-        return s.top();
+        if(s.empty())return -1;
+        if(s.top()<minval)return minval;
+        else return s.top();
+        
     }
-
+    
     int getMin() {
-        if (s.empty()) return -1; // safety
+       
         return minval;
     }
 };
+
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
