febd29b391b61ea02cf2ad801c4af2a4ff87d690
dhruvu340 <dhruvu3400@gmail.com>
2026-03-26 14:53:31 +0530

    Time: 3 ms (9.61%) | Memory: 19.2 MB (5.25%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    vector<int>dp=vector<int>(1001,-1);
-    int solve(vector<int>&cost,int i){
-        if(i>=cost.size())return 0;
-        if(dp[i]!=-1)return dp[i];
-
-        return dp[i] = cost[i] + min(solve(cost,i+1),solve(cost,i+2));
-    }
+    
     int minCostClimbingStairs(vector<int>& cost) {
-      
-        return min(solve(cost,0),solve(cost,1));
+        vector<int>dp(1001,-1);
+        auto solve= [&](auto&& self, int i ) {
+            if(i>=cost.size())return 0;
+            if(dp[i]!=-1)return dp[i];
+
+            return dp[i] = cost[i] + min (self(self,i+1),self(self , i+2));
+        };  
+
+        return min(solve(solve , 0) , solve(solve , 1));
+
     }
 };
