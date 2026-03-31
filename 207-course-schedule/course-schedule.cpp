fc4f74f38b30adb9f41be6faa61c831b17b733e7
dhruvu340 <dhruvu3400@gmail.com>
2026-03-31 12:36:53 +0530

    Time: 7 ms (47.01%) | Memory: 20.3 MB (19.01%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool iscycle(vector<vector<int>>&g,int node,vector<bool>&vis,vector<bool>&pathvis){
+        vis[node]=true;
+        pathvis[node]=true;
+        for(auto i:g[node]){
+            if(!vis[i]){
+                if(iscycle(g,i,vis,pathvis)){
+                    return true;
+                }
+            }
+            else if(pathvis[i]){
+                return true;
+            }
+        }
+
+        pathvis[node]=false;
+        return false;
+
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>g(numCourses);
+        vector<bool>vis(numCourses,false);
+        vector<bool>pathvis(numCourses,false);
+        for(auto i:prerequisites){
+            g[i[1]].push_back(i[0]);
+        }
+
+        for(int i=0;i<numCourses;i++){
+           if(!vis[i]){
+             if(iscycle(g,i,vis,pathvis)){
+                return false;
+            }
+           }
+        }
+
+
+        return true;
+    }
+};
