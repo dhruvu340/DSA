ff32eaf898152465b40527795eab2a837ae8cfb2
dhruvu340 <dhruvu3400@gmail.com>
2026-04-06 02:42:51 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (69.78%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    const long long MOD = 1e9+7;
 
-    long long binaryexpoIterative(long long base, long long n){
-        long long res = 1;
-        base %= MOD;
+    
+    const long long MOD=1e9+7;
+    long long binaryexpoIterative(long long base,long long n){
+    long long res = 1;
 
-        while(n > 0){
-            if(n & 1){
-                res = (res * base) % MOD;
-            }
-            base = (base * base) % MOD;
-            n >>= 1;
+    base = base % MOD;
+
+    while(n>0){
+        if(n&1){
+            res = (res*base)%MOD ;
         }
-        return res;
+
+        base=(base*base)%MOD;
+
+        n = n>>1;
     }
+
+    return res;
+}
 
     int countGoodNumbers(long long n) {
         long long even = (n+1)/2;
-        long long odd = n/2;
+        long long odd=   n/2;
 
-        long long optionodd = binaryexpoIterative(4, odd);
-        long long optioneven = binaryexpoIterative(5, even);
+        long long optionsodd = binaryexpoIterative(4,odd);
+        long long optioneven = binaryexpoIterative(5,even);
 
-        return (optionodd * optioneven) % MOD;
+        return ((optionsodd%MOD)*1LL*(optioneven%MOD))%MOD;
     }
 };
