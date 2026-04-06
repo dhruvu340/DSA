9dfed549e02daff5888788eb48314ee1f6b3c422
dhruvu340 <dhruvu3400@gmail.com>
2026-04-06 22:30:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (85.53%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int dp[46];
-    int solve(int idx){
-        if(idx<0)return 0;
-        if(idx==0)return 1;
-        if(dp[idx]!=-1)return dp[idx];
-        return dp[idx] = solve(idx-1) + solve(idx-2);
-    }
+   int solve(int n){
+    if(n==0)return 1;
+    if(n<0)return 0;
+    if(dp[n]!=-1)return dp[n];
+    return dp[n] = solve(n-1) + solve(n-2);
+   }
     int climbStairs(int n) {
         memset(dp,-1,sizeof(dp));
-       return solve(n);
-
+      return solve(n);
     }
 };
