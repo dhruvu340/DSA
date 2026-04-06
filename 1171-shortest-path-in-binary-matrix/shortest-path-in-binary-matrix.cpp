430f11c33bcea8d6df930f818ced8b1b76db24a3
dhruvu340 <dhruvu3400@gmail.com>
2026-04-06 17:41:01 +0530

    Time: 90 ms (11.40%) | Memory: 31.1 MB (16.68%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -3,31 +3,30 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        
+        vector<vector<int>> dist(n,vector<int>(m,1e9));
         vector<pair<int,int>>dir={{0,1},{1,0},{1,1},{1,-1},{-1,1},{-1,-1},{-1,0},{0,-1}};
         if(grid[0][0]!=0)return -1;
-        queue<vector<int>>q;
-        q.push({0,0,1});
+
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>q;
+        q.push({1,0,0});
+        dist[0][0]=1;
         while(!q.empty()){
-            int size=q.size();
-            while(size--){
-                int i=q.front()[0];
-                int j=q.front()[1];
-                int distance=q.front()[2];
+         
+                int distance=q.top()[0];
+                int i=q.top()[1];
+                int j=q.top()[2];
                 q.pop();
                 if(i==n-1&&j==m-1)return distance;
-                for(auto d:dir){
+                if(distance>dist[i][j])continue;
+                for(auto &d:dir){
                     int x=i+d.first;
                     int y=j+d.second;
-                    
-                    if(x<0||x>=n||y>=m||y<0||grid[x][y]!=0){
-                        continue;
-                    }else{
-                        q.push({x,y,distance+1});
-                        grid[x][y]=-1;
+                    if(x>=0&&y>=0&&y<m&&x<n&&grid[x][y]==0&&(distance+1<dist[x][y])){
+                        dist[x][y]=distance+1;
+                        q.push({dist[x][y],x,y});
                     }
                 }
-            }
+            
         }
 
 
