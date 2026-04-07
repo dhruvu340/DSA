15c8f477a58662af9e64a57fc200e5be555b35a5
dhruvu340 <dhruvu3400@gmail.com>
2026-04-07 09:24:16 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (23.48%) - LeetSync

--- 3188-find-champion-i/find-champion-i.cpp
+++ 3188-find-champion-i/find-champion-i.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int findChampion(vector<vector<int>>& grid) {
-        
+        int winner=0;
         for(int i=0;i<grid.size();i++){
-            int ct=0;
-            for(int j=0;j<grid[0].size();j++){
-                if(i!=j&&grid[i][j]==1){
-                   ct++;
-                }
+            if(winner==i)continue;
+            if(grid[i][winner]==1){
+                winner=i;
             }
-            if(ct==grid[0].size()-1){
-                return i;
-            }
-        }
+        }   
         
 
-        return -1;
+        return winner;
     }
 };
