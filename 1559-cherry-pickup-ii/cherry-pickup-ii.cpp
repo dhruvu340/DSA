dbf85f3540f2526a6b05498ad39f561a823bb71e
dhruvu340 <dhruvu3400@gmail.com>
2026-04-09 21:55:57 +0530

    Time: 33 ms (88.40%) | Memory: 20.9 MB (53.23%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,30 +1,48 @@
 class Solution {
 public:
-    vector<vector<vector<int>>>dp = vector<vector<vector<int>>>(71,vector<vector<int>>(71,vector<int>(71,-1)));
-    int solve(vector<vector<int>>&grid,int row , int c1,int c2,int n,int m){
-        if(row>=n)return 0 ; 
-        if(dp[row][c1][c2]!=-1)return dp[row][c1][c2];
-        int ans = grid[row][c1];
-        if(c1!=c2){
-            ans+=grid[row][c2];
-        }
-        int cherry=0;
-        for(int i = -1 ; i<=1;i++ ){
-            for(int j=-1;j<=1;j++){
-                int nc1=c1+i;
-                int nc2=c2+j;
-                if(nc1>=0&&nc1<m&&nc2>=0&&nc2<m){
-                    cherry = max(cherry,solve(grid,row+1,nc1,nc2,n,m));
-                }
-            }
-        }
-
-
-        return dp[row][c1][c2] =  ans + cherry;
-    }
+    
+   
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        return solve(grid,0,0,m-1,n,m);
+       vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m)));
+       for(int i=0;i<m;i++){
+        for(int j=0;j<m;j++){
+            dp[n-1][i][j]=grid[n-1][i];
+            if(i!=j){
+                dp[n-1][i][j]+=grid[n-1][j];
+            }
+        }
+       }
+
+       for(int i=n-2;i>=0;i--){
+        for(int c1=0;c1 <= min(m-1,i);c1++){
+            for(int c2=max(m-i-1,0);c2<m;c2++){
+                dp[i][c1][c2]=0;
+                for(int prevc1=max(0,c1-1);prevc1<=min(m-1,c1+1);prevc1++){
+                    for(int prevc2=max(0,c2-1);prevc2<=min(m-1,c2+1);prevc2++){
+                        dp[i][c1][c2]=max(dp[i][c1][c2],dp[i+1][prevc1][prevc2]);
+                    }
+                }
+
+                dp[i][c1][c2]+=grid[i][c1];
+                if(c1!=c2){
+                    dp[i][c1][c2]+=grid[i][c2];
+                }
+            }
+        }
+       }
+
+
+
+       int maxans=0;
+       for(int i=0;i<m;i++){
+        for(int j=0;j<m;j++){
+            maxans=max(maxans,dp[0][i][j]);
+        }
+       }
+
+
+       return maxans;
     }
 };
