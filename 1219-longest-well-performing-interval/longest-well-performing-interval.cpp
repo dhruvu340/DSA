da927aa5b0a1ba5f25ddeea38a1579d643ea1a6c
dhruvu340 <dhruvu3400@gmail.com>
2026-04-09 08:32:34 +0530

    Time: 19 ms (13.10%) | Memory: 27 MB (64.56%) - LeetSync

--- 1219-longest-well-performing-interval/longest-well-performing-interval.cpp
+++ 1219-longest-well-performing-interval/longest-well-performing-interval.cpp
@@ -2,13 +2,24 @@ class Solution {
 public:
     int longestWPI(vector<int>& hours) {    
         int ans=0;
+        map<int,int>m;
+        int curr=0;
         for(int i=0;i<hours.size();i++){
-            int curr=0;
-            for(int j=i;j<hours.size();j++){
-                if(hours[j]>8)curr++;
+            if(hours[i]>8){
+                curr++;
+            }else{
+                curr--;
+            }
 
-                if(curr>(j-i+1)/2){
-                    ans=max(ans,j-i+1);
+            if(curr>0){
+                ans=i+1;
+            }else {
+                if(m.find(curr)==m.end()){
+                    m[curr]=i;
+                }
+
+                if(m.find(curr-1)!=m.end()){
+                    ans=max(ans,i-m[curr-1]);
                 }
             }
         }
