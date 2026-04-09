e11c221429966bdfcb555d9247d8a8fd5004c218
dhruvu340 <dhruvu3400@gmail.com>
2026-04-10 01:30:03 +0530

    Time: 814 ms (13.89%) | Memory: 321.8 MB (5.10%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>>dp=vector<vector<int>>(21,vector<int>(200001,-1));
+    int solve(vector<int>&nums,int target,int sum,int i){
+        if(i==nums.size()){
+            if(sum==target){
+               
+                return dp[i][10000+sum] = 1;
+                
+                }
+            else return dp[i][10000+sum] = 0;
+        }
+
+        if(dp[i][10000+sum]!=-1)return dp[i][10000+sum];
+
+        int add = sum + nums[i];
+        int minus = sum - nums[i];
+
+        int f=solve(nums,target,add,i+1);
+        int e=solve(nums,target,minus,i+1);
+
+
+        return dp[i][10000+sum] = f+e;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return solve(nums,target,0,0);
+    }
+};
