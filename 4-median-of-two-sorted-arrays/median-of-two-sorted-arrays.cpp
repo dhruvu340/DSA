a5fca5971e646811c06f40cb12ff8da32befb718
dhruvu340 <dhruvu3400@gmail.com>
2026-04-12 15:53:30 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (61.40%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size()>nums2.size()){
-          return findMedianSortedArrays(nums2,nums1); 
-        }
-        int m=nums1.size();
-        int n=nums2.size();
-        int l=0;
-        int r=m;
+        int n=nums1.size();
+        int m=nums2.size();
+        if(n>m)return findMedianSortedArrays(nums2,nums1);
+        int l = 0;
+        int r = n;
+
         while(l<=r){
-            int mid = l + ( r - l )/2;
-            int rest = (m + n + 1)/2 - mid;
+            int mid = l + (r-l)/2;
+            int rest = (m+n+1)/2-mid;
+            int x1 = (mid==0)?INT_MIN:nums1[mid-1];
+            int x2=(rest==0)?INT_MIN:nums2[rest-1];
+            int x3=(mid==n)?INT_MAX:nums1[mid];
+            int x4=(rest==m)?INT_MAX:nums2[rest];
 
-            int x1=(mid==0)?INT_MIN:nums1[mid-1];
-            int x2 = (rest==0)?INT_MIN:nums2[rest-1];
-            int r1 = (mid==m)?INT_MAX:nums1[mid];
-            int r2=(rest==n)?INT_MAX:nums2[rest];
-
-            if(x1<=r2&&x2<=r1){
+            if(x1<=x4&&x2<=x3){
                 if((m+n)%2==0){
-                    return (max(x1,x2)+min(r1,r2))/2.0;
+                    return (max(x1,x2)+min(x3,x4))/2.0;
                 }else{
                     return max(x1,x2);
                 }
             }
 
-            if(x1>r2){
+            if(x1>x4){
                 r=mid-1;
-            }else if(x2>r1){
+            }else if(x2>x3){
                 l=mid+1;
             }
         }
