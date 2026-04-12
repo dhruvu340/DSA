336f76ac61b553c54776c05d0f3196aa290001da
dhruvu340 <dhruvu3400@gmail.com>
2026-04-12 15:36:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (67.56%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
-int counts(vector<int>&a,int x){
-    int students=1;
-    long long pages=0;
-    for(int i=0;i<a.size();i++){
-        if(pages+a[i]<=x){
-            pages+=a[i];
-        }else{
-            pages=a[i];
-            students++;
-        }
-    }
-    return students;
-}
     int splitArray(vector<int>& nums, int k) {
-
-        if(nums.size()<k){
-            return -1;
-        }
-
         int l=*max_element(nums.begin(),nums.end());
-        int r=accumulate(nums.begin(),nums.end(),0,[](int a,int b){
-            return a+b;
-        });
-
-        while(l<r){
-            int mid= l+(r-l)/2;
-            int st=counts(nums,mid);
-            if(st<=k){r=mid;}
-            else{
-                l=mid+1;
+        int r=accumulate(nums.begin(),nums.end(),0);
+        int ans=-1;
+        auto checker = [&](int mid){
+            int ct = 0;
+            int partitions=1;
+            for(auto i:nums){
+                if(ct+i<=mid){
+                    ct+=i;
+                }else{
+                    partitions++;
+                    ct=i;
+                }
             }
+            return partitions<=k;
+        };
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(checker(mid)){
+                ans=mid;
+                r=mid-1;
+            }
+            else l=mid+1;
         }
-
-        return l;
-
-        
+        return ans;
     }
 };
