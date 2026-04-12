f32bab5058993bf96ded6806660c582b8a96d539
dhruvu340 <dhruvu3400@gmail.com>
2026-04-12 08:50:17 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (25.81%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool search(vector<int>& a, int target) {
+        int l=0;
+        int r=a.size()-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(a[mid]==target)return true;
+             if(a[mid]==a[l]&&a[mid]==a[r]){
+                r--;
+                l++;
+
+            }else if(a[l]<=a[mid]){
+                if(a[l]<=target&&target<=a[mid])r=mid-1;
+                else l=mid+1;
+            }else{
+                if(a[r]>=target&&target>=a[mid])l=mid+1;
+                else r=mid-1;
+            }
+
+        }
+        return false;
+    }
+};
