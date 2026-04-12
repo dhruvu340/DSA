b22f63e58340aa28464d9fc1a8229f584206b3e5
dhruvu340 <dhruvu3400@gmail.com>
2026-04-12 18:09:37 +0530

    Time: 103 ms (42.93%) | Memory: 324.7 MB (14.66%) - LeetSync

--- 2618-maximize-the-minimum-powered-city/maximize-the-minimum-powered-city.cpp
+++ 2618-maximize-the-minimum-powered-city/maximize-the-minimum-powered-city.cpp
@@ -1,53 +1,51 @@
-using ll = long long;
-
+using ll=long long; 
 class Solution {
 public:
-    bool checker(ll mid, vector<ll> diff, ll n, ll k , ll r){
-        ll cumm = 0;
+    bool checker(ll mid,vector<ll>diff,ll n,ll k ,ll r){
+        ll cumm=0;
         
-        for(int i = 0; i < n; i++){
-            cumm += diff[i];
-
-            if(cumm < mid){
-                ll need = mid - cumm;
-                if(need > k) return false;
-
-                k -= need;
-                cumm += need;
-
-                if(i + 2*r + 1 < n){
-                    diff[i + 2*r + 1] -= need;
+        for(int i=0;i<n;i++){
+            cumm+=diff[i];
+            if(cumm<mid){
+                ll need=mid-cumm;
+                if(need>k){
+                    return false;
                 }
+                k-=need;
+                
+                if(i+2*r+1<n){
+                    diff[i+2*r+1]-=need;
+                }
+                cumm+=need;
             }
         }
 
         return true;
     }
-
     long long maxPower(vector<int>& stations, int r, int k) {
-        ll n = stations.size();
-        vector<ll> diff(n, 0);
-
-        for(int i = 0; i < n; i++){
-            diff[max(0, i - r)] += stations[i];
-            if(i + r + 1 < n){
-                diff[i + r + 1] -= stations[i];
+        ll n=stations.size();
+        vector<ll>diff(n,0);
+        for(int i=0;i<n;i++){
+            diff[max(0,i-r)]+=stations[i];
+            if(i+r+1<n){
+                diff[i+r+1]-=stations[i];
             }
         }
 
-        ll left = 0, right = 1e14, ans = 0;
+        
 
-        while(left <= right){
-            ll mid = left + (right - left) / 2;
-
-            if(checker(mid, diff, n, k, r)){
-                ans = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        ll left=0;
+        ll right=1e18;
+        ll ans=-1;
+        while(left<=right){
+            ll mid=left+(right-left)/2;
+            if(checker(mid,diff,n,k,r)){
+                left=mid+1;
+                ans=mid;
+            }else{
+                right=mid-1;
             }
         }
-
         return ans;
     }
 };
