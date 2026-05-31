5ef0359a52ff03689e93fb992da6ba37f588f878
dhruvu340 <dhruvu3400@gmail.com>
2026-05-31 09:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (20.76%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        int maxel = *max_element(arr1.begin(),arr1.end());
-        vector<int>count(maxel+1);
-        vector<int>ans;
-        for(auto i:arr1)count[i]++;
-        for(auto i:arr2){
-            while(count[i]>0){
+        int maxel = *max_element(arr1.begin(), arr1.end());
+        vector<int> ct(maxel + 1, 0);
+        for (auto i : arr1)
+            ct[i]++;
+        vector<int> ans;
+        for (auto i : arr2) {
+            while (ct[i] > 0) {
                 ans.push_back(i);
-                count[i]--;
+                ct[i]--;
             }
         }
-        for(int i=0;i<=maxel;i++){
-            while(count[i]>0){
+        for (int i = 0; i <= maxel; i++) {
+            while (ct[i] > 0) {
                 ans.push_back(i);
-                count[i]--;
+                ct[i]--;
             }
         }
         return ans;
