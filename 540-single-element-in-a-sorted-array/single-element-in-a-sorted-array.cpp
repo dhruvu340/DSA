e7e9f075b6d187f0aba735d0f49c9825897517ed
dhruvu340 <dhruvu3400@gmail.com>
2026-06-01 10:48:50 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (33.35%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -5,14 +5,16 @@ public:
         if(n==1)return nums[0];
         if(nums[0]!=nums[1])return nums[0];
         if(nums[n-1]!=nums[n-2])return nums[n-1];
-        int l=1;
-        int r=n-2;
+        int l=0;
+        int r=n-1;
         while(l<=r){
             int mid=l+(r-l)/2;
-            if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1])return nums[mid];
-            else if ((mid % 2 == 0 && nums[mid] == nums[mid+1]) ||
-    (mid % 2 == 1 && nums[mid] == nums[mid-1]))l=mid+1;
-            else r=mid-1;
+            if(nums[mid]!=nums[mid-1]&&nums[mid+1]!=nums[mid])return nums[mid];
+            else if((mid%2==0&&nums[mid]==nums[mid+1])||(mid%2!=0&&nums[mid]==nums[mid-1])){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
         }
         return -1;
     }
