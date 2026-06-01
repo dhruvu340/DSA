b25741a08c839aa1b0913c604fdfb07fd9da1ab2
dhruvu340 <dhruvu3400@gmail.com>
2026-06-01 22:59:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (53.46%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
+    int dp[46];
+    int solve(int n){
+        if(n==0) return 1;
+        if(n<0)return 0;
+        if(dp[n]!=-1)return dp[n];
+        return dp[n] = solve(n-1) + solve(n-2);
+    }
     int climbStairs(int n) {
-        if(n<=1)return 1;
-        //if n is less than or equal to 1 then there is only one possibility
-        vector<int>dp(n+1);
-
-        //base case is when if n < = 1 return 1;
-        dp[0]=1;
-        dp[1]=1;
-
-        // transition function will look alike this dp[i] = dp[i-1] + dp[i-2];
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
     }
 };
