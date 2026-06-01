cccf8f6982a0ead48a7776356b46259f97c0bbdf
dhruvu340 <dhruvu3400@gmail.com>
2026-06-01 09:52:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (78.12%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,18 +4,12 @@ public:
         int n=nums.size();
         int m=nums[0].size();
         int l=0;
-        int r=m*n-1;
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            int row=(mid/m);
-            int col=mid%m;
-            if(nums[row][col]==target)return true;
-            else if(nums[row][col]>target)r=mid-1;
-            else l=mid+1;
+        int r=m-1;
+        while(l<n&&r<m&&l>=0&&r>=0){
+           if(nums[l][r]==target)return true;
+           else if(nums[l][r]>target)r--;
+           else l++;
         }
-
-        return false;
-
-        
+        return false;   
     }
 };
