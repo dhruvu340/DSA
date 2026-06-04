42f99d3313d0e7906e799db0482f618f7a5f46ee
dhruvu340 <dhruvu3400@gmail.com>
2026-06-04 08:56:03 +0530

    Time: 7 ms (12.78%) | Memory: 25.9 MB (5.38%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-    void dfs(int node ,map<int,vector<int>>&adj, vector<bool>&vis){
-        if(vis[node]==true)return ;
-
-        vis[node]=true;
-        for(auto i:adj[node]){
-           if(!vis[i]){ dfs(i,adj,vis);}
-        }
-
-    }
-    int findCircleNum(vector<vector<int>>& a) {
-        map<int,vector<int>>adj;
-        int V=a.size();
-        for(int i=0;i<a.size();i++){
-            for(int j=0;j<a[0].size();j++){
-                if(a[i][j]==1&&i!=j){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+    void bfs(unordered_map<int,vector<int>>graph,vector<bool>&vis,int i){
+        queue<int>q;
+        q.push(i);
+        vis[i] = true;
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                int top = q.front();
+                q.pop();
+                for(auto i : graph[top]){
+                    if(!vis[i]){vis[i] = true;
+                    q.push(i);}
                 }
             }
         }
-        vector<bool>vis(V,false);
-        int ct=0;
-        for(int i=0;i<V;i++){
-            if(vis[i]!=true){
-                dfs(i,adj,vis);
+
+        
+    }
+    int findCircleNum(vector<vector<int>>& rooms) {
+        unordered_map<int,vector<int>>graph;
+        for(int i=0;i<rooms.size();i++){
+            for(int j=0;j<rooms[i].size();j++){
+                if(rooms[i][j]==1){graph[i].push_back(j);
+                graph[i].push_back(j);}
+            }
+        }
+        int n=rooms.size();
+        vector<bool>vis(n,false);
+
+        int ct =0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                bfs(graph,vis,i);
                 ct++;
             }
         }
+
+
 
         return ct;
     }
