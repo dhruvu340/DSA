fa36428483487b8695a1f9ec0baa8d0708919336
dhruvu340 <dhruvu3400@gmail.com>
2026-06-07 02:40:51 +0530

    Time: 1203 ms (5.01%) | Memory: 16.4 MB (68.26%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<int>>dp(amount+1,vector<int>(n,0));
-        for(int i=0;i<n;i++){
-            dp[0][i]=1;
-        }
-        if(coins[0]<=amount)dp[coins[0]][0]=1;
-
-        for(int i=1;i<=amount;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j] = ((j-1>=0) ? dp[i][j-1] : 0);
-                if(i-coins[j]>=0)dp[i][j]+=1LL*dp[i-coins[j]][j];
+    int dp[5001][301];
+    int solve(int amount ,vector<int>&coins,int i){
+        if(amount==0)return 1;
+        if(dp[amount][i]!=-1)return dp[amount][i];
+        int ans =0;
+        for(int idx=i;idx<coins.size();idx++){
+            if(coins[idx]<=amount){
+                ans += solve(amount - coins[idx] , coins,idx);
             }
         }
-        return dp[amount][n-1];
+        return dp[amount][i] = ans;
+    }
+    int change(int amount, vector<int>& coins) {
+        memset(dp,-1,sizeof(dp));
+        return solve(amount,coins,0);
     }
 };
