bc33d3f7d4b85dc89bd0b7f001ae0580450ad521
dhruvu340 <dhruvu3400@gmail.com>
2026-06-07 00:40:23 +0530

    Time: 427 ms (22.94%) | Memory: 210.9 MB (74.56%) - LeetSync

--- 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    long long dp[100001];
-    long long solve(vector<int>&v,int i,map<int,int>&m){
-        if(i>=v.size())return 0;
-        if(dp[i]!=-1)return dp[i];
-        int tkid=upper_bound(v.begin(),v.end(),v[i]+2)-v.begin();
-        return dp[i]=max(solve(v,i+1,m)*1LL,v[i]*1LL*m[v[i]]+solve(v,tkid,m));
-    }
-    long long maximumTotalDamage(vector<int>& v) {
-        memset(dp,-1,sizeof(dp));
-       sort(v.begin(),v.end());
-        map<int,int>m;
-        for(auto i:v)m[i]++;
-        return solve(v,0,m);
+    long long maximumTotalDamage(vector<int>& power) {
+        int n = power.size();
+        vector<long long>dp(n);
+        sort(power.begin(),power.end());
+        map<int,int>mpp;
+        for(auto i:power)mpp[i]++;
+        dp[n-1] = power[n-1];
+        for(int i=n-2;i>=0;i--){
+            dp[i] = dp[i+1];
+            int gridx = upper_bound(power.begin()+i,power.end(),power[i]+2) - power.begin();
+           
+                dp[i] = max(dp[i],power[i]*1LL*mpp[power[i]]+ ((gridx<n)?1LL*dp[gridx]:0));
+            
+        }
+        return dp[0];
     }
 };
