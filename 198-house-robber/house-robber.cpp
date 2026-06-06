9da7f223c18c3ae49539af84cb584e989b69d38f
dhruvu340 <dhruvu3400@gmail.com>
2026-06-06 23:44:39 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (21.15%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    // adjacent houses not allowed
-   
-    int rob(vector<int>& nums) {int n=nums.size();
-    if(n<2)return nums[0];
-    vector<int>dp(n);
-    dp[0]=nums[0];
-    dp[1]=max(dp[0],nums[1]);
-    for(int i=2;i<n;i++){
-        dp[i]=max(dp[i-1],nums[i]+dp[i-2]);
-    }
-
-    return dp[n-1];
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)return nums[0];
+        vector<int>dp(n+1);
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i=2;i<=n;i++){
+            dp[i] = max(dp[i-1],dp[i-2] + nums[i-1]);
+        }
+        return dp[n];
     }
 };
