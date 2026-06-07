f4f4a637b6f0859b6776355a3d7ea74180eb848c
dhruvu340 <dhruvu3400@gmail.com>
2026-06-07 15:37:26 +0530

    Time: 95 ms (61.20%) | Memory: 57.8 MB (36.13%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -3,23 +3,18 @@ public:
     unordered_map<int,int>m;
     bool canCross(vector<int>& stones) {
         int n=stones.size();
-        vector<vector<int>>dp(stones.size(),vector<int>(n,false));
-        for(int i=0;i<n;i++)dp[n-1][i] = true;
-        for(int i=0;i<n;i++)m[stones[i] ]=i;
-        if(stones[1]!=1)return false;
-        for(int i = n-2;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                dp[i][j]=false;
-                for(int diff = j-1;diff<=j+1;diff++){
-                 if(diff>0&&diff<n&&m.find(stones[i] + diff)!=m.end()){
-                    dp[i][j]|= dp[m[stones[i] + diff]][diff];
-                    }
-
-                }
+        unordered_map<int,set<int>>m;
+        m[1] = {1};
+        for(int i = 1;i<n;i++){
+            for(auto diff:m[stones[i]]){
+                m[stones[i] + diff].insert(diff);
+                m[stones[i] + diff+1].insert(diff+1);
+                m[stones[i] + diff-1].insert(diff-1);
             }
         }
 
-        return dp[1][1];
+
+        return m[stones[n-1]].size() ? true : false;
 
     }
 };
