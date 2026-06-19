75bb02513c2af3ddf7eed27e4759e7daeb71eb81
dhruvu340 <dhruvu3400@gmail.com>
2026-06-19 21:03:47 +0530

    Time: 3 ms (32.61%) | Memory: 14.7 MB (22.83%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<long long>v(1001,0);
+        vector<int>diff(1002,0);
         for(auto i:trips){
-            int add=i[0];
-            int st=i[1];
-            int end=i[2];
-            v[st]+=add;
-            if(end<1001){
-                v[end]-=add;
+            diff[i[1]] += i[0];
+            diff[i[2]] -= i[0];
+        }
+
+        for(int i=1;i<1002;i++)diff[i] += diff[i-1];
+
+        bool ans = true;
+        for(auto i:diff){
+            if(i>capacity){
+                ans=false;
+                break;
             }
         }
 
-        for(int i=1;i<1001;i++){
-            v[i]+=1LL*v[i-1];
-        }
 
-        for(auto i:v){
-            if(i>capacity)return false;
-        }
-
-        return true;
+        return ans;
     }
 };
