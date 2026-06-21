0f8baec55618f76786c0c484ba8216bfb43ee4d6
dhruvu340 <dhruvu3400@gmail.com>
2026-06-21 14:55:28 +0530

    Time: 448 ms (71.53%) | Memory: 154.5 MB (71.39%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
@@ -3,51 +3,29 @@ public:
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>>prefix(n,vector<int>(m));
-        prefix[0][0] = matrix[0][0];
-        for(int i=1;i<n;i++){
-            prefix[i][0] = prefix[i-1][0] + matrix[i][0];
-        }
-        for(int i=1;i<m;i++){
-            prefix[0][i] = prefix[0][i-1] + matrix[0][i];
-        }
-
-        for(int i = 1;i<n;i++){
-            for(int j=1;j<m;j++){
-                prefix[i][j] = matrix[i][j] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1];
+        for(int i = 0;i<n;i++){
+            for(int j = 1;j<m;j++){
+                matrix[i][j] += matrix[i][j-1];
             }
         }
         
 
         int ans = 0;
-        for(int i = 0;i<n;i++){
-            for(int rs = 1;i+rs<=n;rs++){
-                for(int j = 0;j<m;j++){
-                    for(int cs=1;j+cs<=m;cs++){
-                        int r1 = i;
-                        int c1 = j;
 
-                        int r2 = i + rs - 1;
-                        int c2 = j + cs - 1;
-
-                        int sum = prefix[r2][c2];
-                        if(r1-1>=0){
-                            sum -= prefix[r1-1][c2];
-                        }
-                        if(c1-1>=0){
-                            sum -= prefix[r2][c1-1];
-                        }
-
-                        if(r1-1>=0 && c1-1>=0){
-                            sum += prefix[r1-1][c1-1];
-                        }
-
-                        if(sum == target)ans++;
+        for(int c = 0 ; c<m;c++){
+            for(int j = c;j<m;j++){
+                unordered_map<int,int>m;
+                m[0] = 1;
+                int sum = 0;
+                for(int i = 0;i<n;i++){
+                    sum += matrix[i][j] - ((c>0) ? matrix[i][c-1] : 0);
+                    if(m.find(sum - target) != m.end()){
+                        ans += m[sum-target]; 
                     }
+                    m[sum]++;
                 }
             }
         }
-
         return ans;
     }
 };
