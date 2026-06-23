e00f5402914424473608440640a3154c70cac1f2
dhruvu340 <dhruvu3400@gmail.com>
2026-06-23 10:15:54 +0530

    Time: 2 ms (72.58%) | Memory: 102.3 MB (92.03%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int minOperations(vector<int>& v, int x) {
-        int n = v.size();
-        int sumtofind = accumulate(v.begin(),v.end(),0) - x;
-        int l = 0;
+        int n =v.size();
         int ans = INT_MAX;
-        int sum = 0;
-        int i;
-        for( i=0;i<n;i++){
-            sum += v[i];
-            while(l<=i&&sum>sumtofind){
-                
+        int sum  = 0;
+        int toFind = accumulate(v.begin(),v.end(),0) - x;
+        for(int l = 0, r = 0 ;r<n;r++){
+            sum += v[r];
+            while(l<=r&&sum>toFind){
                 sum-=v[l];
                 l++;
             }
-            if(sum==sumtofind){
-            ans = min(ans,n-(i-l+1));
-        }
+            if(sum == toFind){
+                ans = min(ans,n - (r-l+1));
+            }
         }
         
-        return (ans == INT_MAX) ? -1 : ans;
+        return ans == INT_MAX ? -1 : ans;
     }
 };
