905acf922edd566d11340184d5901562e2da4366
dhruvu340 <dhruvu3400@gmail.com>
2026-06-24 14:34:08 +0530

    Time: 14 ms (88.46%) | Memory: 93.2 MB (34.73%) - LeetSync

--- 164-maximum-gap/maximum-gap.cpp
+++ 164-maximum-gap/maximum-gap.cpp
@@ -1,12 +1,34 @@
 class Solution {
 public:
-    int maximumGap(vector<int>& nums) {
-        int n=nums.size();
-        int ans = 0;
-        sort(nums.begin(),nums.end());
-        for(int i = 0;i<n-1;i++){
-            ans=max(ans,abs(nums[i] - nums[i+1]));
+    int maximumGap(vector<int>& v) {
+    // bucket sort 
+    int n = v.size();
+    if(n==1)return 0;
+    int mini = *min_element(v.begin(),v.end());
+    int maxi = *max_element(v.begin(),v.end());
+    double bucket = (maxi - mini)*1.0/(n-1);
+    if(maxi==mini)return 0;
+    if(bucket < 1)bucket = 1.0;
+    vector<int>vmax(n,INT_MIN),vmin(n,INT_MAX);
+    if(bucket == 0)bucket = 1;
+    for(int i = 0 ; i < n ; i++ ){
+        int pos =  (v[i] - mini)/bucket;
+        vmax[pos] = max(vmax[pos],v[i]);
+        vmin[pos] = min(vmin[pos],v[i]);
+    }
+    int  i=0;
+    int j =1;
+    int ans = 0;
+    while(j<n){
+        if(vmin[j] == INT_MAX)j++;
+        else if(vmax[i] == INT_MIN)i++;
+        else{
+            ans = max(ans, vmin[j] - vmax[i]);
+            i++;
+            j++;
         }
-        return ans;
+    }
+
+    return ans;
     }
 };
