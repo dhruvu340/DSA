659f401b74e234cb6cd61ece70dd4e14fb7c222d
dhruvu340 <dhruvu3400@gmail.com>
2026-07-02 20:20:51 +0530

    Time: 4 ms (8.44%) | Memory: 14.4 MB (88.40%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    void dfs (vector<int>&vis,unordered_map<int,vector<int>>&gr,int i){
-        vis[i] = true;
-        for(auto a:gr[i]){
-            if(!vis[a]){
-                dfs(vis,gr,a);
+    void bfs(int node ,vector<vector<int>>&adj,vector<bool>&vis){
+        queue<int>q;
+        q.push(node);
+        vis[node]=true;
+        while(!q.empty()){
+            int size=q.size();
+            while(size--){
+                int nextElement = q.front();
+                q.pop();
+                for(auto i:adj[nextElement]){
+                    if(!vis[i]){
+                        vis[i] = true;
+                        q.push(i);
+                    }
+                }
             }
         }
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        unordered_map<int,vector<int>>graph;
-        for(int i=0;i<rooms.size();i++){
-            for(auto j:rooms[i]){
-                graph[i].push_back(j);
-            }
-        }
-        int n=rooms.size();
-        vector<int>vis(n,false);
-        dfs(vis,graph,0);
+        vector<bool>vis(rooms.size(),false);
+        bfs(0,rooms,vis);
         for(auto i:vis){
             if(!i)return false;
         }
