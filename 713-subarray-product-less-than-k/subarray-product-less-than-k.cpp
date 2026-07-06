c17bc184a74ca7fd901cf1f50d53d4d6321b6abc
dhruvu340 <dhruvu3400@gmail.com>
2026-07-06 16:27:30 +0530

    Time: 10 ms (12.86%) | Memory: 45.8 MB (95.50%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        int ans = 0;
-        long long p = 1;
+        if(k==0)return 0;
+        double logK = log(k);
         int n = nums.size();
-        for(int l=0,r=0;r<n;r++){
-            p*=nums[r]*1LL;
-            while(l<=r && p>=k){
-                p/=nums[l++];
-            }
-            ans += r-l+1;
+        vector<double>logsPrefixSum(nums.size(),0);
+        logsPrefixSum[0] = log(nums[0]);
+        for(int i = 1;i<nums.size();i++){
+            logsPrefixSum[i] = logsPrefixSum[i-1] + (double)log(nums[i]);
+        }
+        int ans = 0;
+        for(int i = 0 ;i<n;i++){
+            int curr = i-1;
+            double prev = (curr>=0)?logsPrefixSum[curr]:0;
+            double toFind = prev + logK - 1e-9;
+            int lastIdx = upper_bound(logsPrefixSum.begin()+i,logsPrefixSum.end(),toFind)-logsPrefixSum.begin();
+            ans += lastIdx - i;
         }
         return ans;
     }
