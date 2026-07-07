0bde9652d620d8ce5cec3c3b77284b81ed770cd4
dhruvu340 <dhruvu3400@gmail.com>
2026-07-07 09:54:43 +0530

    Time: 127 ms (43.93%) | Memory: 115.8 MB (42.03%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -3,37 +3,34 @@ public:
     int dfs(int node,vector<vector<pair<int,int>>>&adj,vector<int>&vis){
         queue<int>q;
         q.push(node);
-        vis[node] = true;
-        int ct = 0;
+        vis[node]=true;
+        int ans = 0;
         while(!q.empty()){
-            int size=q.size();
+            int size = q.size();
             while(size--){
-                int currElement = q.front();
+                int n = q.front();
                 q.pop();
-                for(auto i:adj[currElement]){
-                    int el = i.first;
-                    int dup = i.second;
-
-                    if(!vis[el]){
-                        vis[el] = true;
-                        ct += dup;
-                        q.push(el);
+                for(auto i:adj[n]){
+                    if(vis[i.first]==false){
+                        vis[i.first]=true;
+                        ans += i.second;
+                        q.push(i.first);
                     }
                 }
             }
         }
-        return ct;
+        return ans;
     }
     int minReorder(int n, vector<vector<int>>& connections) {
         vector<vector<pair<int,int>>>adj(n);
         for(auto i:connections){
             int u = i[0];
             int v = i[1];
-            adj[u].push_back({v,0});
-            adj[v].push_back({u,1});
+            adj[u].push_back({v,1});
+            adj[v].push_back({u,0});
         }
         vector<int>vis(n,0);
         int ct = dfs(0,adj,vis);
-        return connections.size()-ct;
+        return ct;
     }
 };
