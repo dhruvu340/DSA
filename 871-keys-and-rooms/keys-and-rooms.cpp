caca4bded21db8bed29f8ab2a6faa63fa29f14d0
dhruvu340 <dhruvu3400@gmail.com>
2026-07-07 09:29:26 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (54.27%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -2,8 +2,20 @@ class Solution {
 public:
     void traverse(int node,vector<bool>&vis,vector<vector<int>>&graph){
         vis[node] = true;
-        for(auto i:graph[node]){
-            if(!vis[i])traverse(i,vis,graph);
+        queue<int>q;
+        q.push(node);
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                int front = q.front();
+                q.pop();
+                for(auto i:graph[front]){
+                    if(!vis[i]){
+                        vis[i] = true;
+                        q.push(i);
+                    }
+                }
+            }
         }
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
