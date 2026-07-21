42a473b217400ef5f16bb526f3e627b02e4ba688
dhruvu340 <dhruvu3400@gmail.com>
2026-07-21 16:24:56 +0530

    Time: 112 ms (12.53%) | Memory: 131.3 MB (12.34%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     vector<vector<int>>ans;
-    void solve(int n,int k,vector<int>&temp,vector<bool>&vis){
+    void solve(int n,int k,vector<int>&temp,int c){
+        
         if(k<=0){
             ans.push_back(temp);
             return;
         }
 
-        for(int i=1;i<=n;i++){
-            if(!vis[i] &&  (temp.empty()||temp.back()<i)){
-                vis[i]=true;
-                temp.push_back(i);
-                solve(n,k-1,temp,vis);
-                vis[i]=false;
-                temp.pop_back();
-            }
-        }
+        if(c > n)return ;
 
+        temp.push_back(c);
+        solve(n,k-1,temp,c+1);
 
+        temp.pop_back();
+        solve(n,k,temp,c+1);
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<bool>vis(n+1,false);
+        
         vector<int>temp;
-        solve(n,k,temp,vis);
+        solve(n,k,temp,1);
         return ans;
     }
 };
