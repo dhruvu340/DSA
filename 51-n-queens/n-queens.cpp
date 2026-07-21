9b3ce37b52b42d4ab5e44c55564a56d1c4c00f27
dhruvu340 <dhruvu3400@gmail.com>
2026-07-21 17:18:07 +0530

    Time: 43 ms (6.19%) | Memory: 50.1 MB (5.04%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
     vector<vector<string>>ans;
-    bool safe(vector<string>s,int row,int col,int n){
-        for(int i=0;i<n;i++){
-            if(s[row][i]=='Q')return false;
-        }
-        for(int i=0;i<n;i++){
-            if(s[i][col]=='Q')return false;
+    bool safe(int i,int j,vector<string>temp){
+        //col
+        for(int r = i ; r>=0 ;r--){
+            if(temp[r][j]=='Q')return false;
         }
 
-        for(int r=row,c=col;r>=0&&c>=0;r--,c--){
-            if(s[r][c]=='Q')return false;
-        }
-        for(int r=row,c=col;r>=0&&c>=0;r--,c++){
-            if(s[r][c]=='Q')return false;
+        for(int r=i,c=j;r>=0&&c>=0;r--,c--){
+            if(temp[r][c]=='Q')return false;
         }
 
-
+        for(int r=i,c=j;r>=0&&c<temp.size();r--,c++){
+            if(temp[r][c]=='Q')return false;
+        }
         return true;
-
-
     }
-    void solve(vector<string>s,int row,int n){
+    void solve(vector<string> &temp,int row,int n){
         if(row==n){
-            ans.push_back(s);
-            return ;
+            ans.push_back(temp);
+            return;
         }
 
         for(int i=0;i<n;i++){
-            if(safe(s,row,i,n)){
-                s[row][i]='Q';
-                solve(s,row+1,n);
-                s[row][i]='.';
+            if(safe(row,i,temp)){
+                temp[row][i] = 'Q';
+                solve(temp,row+1,n);
+                temp[row][i] = '.';
             }
         }
+
+
+        return ;
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<string>s(n,string(n,'.'));
-        solve(s,0,n);
-
+        
+       vector<string>temp(n,string(n,'.'));
+        solve(temp,0,n);
         return ans;
     }
 };
